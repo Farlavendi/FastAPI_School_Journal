1a8000f61bb5d8c41c0c412430f57b8c76e98567
Denis Bichuk <denisbicuk18@gmail.com>
2025-01-02 15:51:41 +0200

    Dev dependencies.

--- src/core/config.py
+++ src/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # db_url: str = getenv("DB_URL")
-    db_url: str = 'postgresql+asyncpg://postgres@localhost:5432/fastapi_journal'
+    db_url: str = "postgresql+asyncpg://postgres@localhost:5432/fastapi_journal"
     db_echo: bool = True  # IMPORTANT set False on production, True is ONLY for debug!
 
 
